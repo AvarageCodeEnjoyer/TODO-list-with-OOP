5e18223d12615373398d0fbc4b3e57aef4df6b03
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-02-10 18:16:11 -0700

    add Classes

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const form = document.querySelector("[name='data-form']")
-const input = document.querySelector("input[name='data-input']")
-const lists = document.querySelector("input[name='data-lists']")
-let todoARR = []
+const input = document.querySelector("[name='data-input']")
+const lists = document.querySelector("[name='data-lists']")
+// let todoARR = []
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  let id = Math.random() * 17
-  var todo = new ToDo(id, todo)
-  console.log(todo)
-  todoARR.push(todo)
-})
+
+
+/* ------------------------ Construct list item data ------------------------ */
 
 class ToDo{
   constructor(id, todo){
@@ -18,16 +14,69 @@ class ToDo{
   }
 }
 
-class UI{
-  displayData(){
-    let displayData = todoARR.forEach(item => { 
-      item.innerHTML = 
-      `<div class="todo">
-        <p></p>
-        <span class="remove" data-id="${item.id}"> </span>
+/* ------------------------------ local storage ----------------------------- */
+
+class Storage{
+  static addToStorage(todoARR){
+    let storage = localStorage.setItem('todo', JSON.stringify(todoARR))
+    return storage
+  }
+
+  static getStorage(){
+    let storage = localStorage.getItem('todo') === null ? [] : JSON.parse(localStorage.getItem('todo'))
+    return storage
+  }
+}
+
+let todoARR = Storage.getStorage()
+
+/* -------------------------- Display data in list -------------------------- */
+
+class UI {
+  static displayData(){
+    const displayData = todoARR.map(item => { 
+      return `
+      <div class="todo">
+        <p>${item.todo}</p>
+        <span class="remove" data-id="${item.id}">🗑️</span>
       </div>`
     });
 
-    lists.innerHTML = displayData.join("")
+    lists.innerHTML = (displayData).join("")
+  }
+
+  static clearInput(){
+    input.value = ""
+  }
+
+  static removeToDo(){
+    lists.addEventListener('click', e => {
+      if(e.target.classList.contains("remove")){
+        e.target.parentElement.remove()
+      }
+      let btnId = e.target.dataset.id
+      console.log(todoARR)
+      UI.removeArrayTodo(btnId)
+    })
+  }
+
+  static removeArrayTodo(id){
+    todoARR = todoARR.filter((item) => item.id !== id)
+    // filtered = Storage.addToStorage()
+    console.log(todoARR)
+
   }
 }
+
+/* -------------- Add form event listener for static functions -------------- */
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let id = Math.random() * 17
+  const item = new ToDo(id, input.value)
+  todoARR = [...todoARR, item]
+  UI.displayData()
+  UI.removeToDo()
+  UI.clearInput()
+  Storage.addToStorage(todoARR)
+})
